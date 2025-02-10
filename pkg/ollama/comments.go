c948d73bc400a64a2576002f1c8c7e1211fda220
krizz <github@krizz.org>
2025-02-10 21:28:23 +0100

    last sync before breakdown

--- pkg/ollama/comments.go
+++ pkg/ollama/comments.go
@@ -2,25 +2,19 @@ package ollama
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 	"path/filepath"
 	"slices"
 	"strings"
-	"time"
 
 	"github.com/jonathanhecl/gollama"
 	"github.com/k0kubun/pp"
 	"github.com/sourcegraph/go-diff/diff"
 
-	"github.com/lakrizz/prollama/pkg/hunks"
 	"github.com/lakrizz/prollama/pkg/models"
 )
 
 func (o *Ollama) GenerateCommentsForPatch(diffs []*diff.FileDiff) ([]*models.Comment, error) {
-	logFolder := filepath.Join("logs", fmt.Sprintf("%v", time.Now().Unix()))
 	ctx := context.Background()
 
 	res := make([]*models.Comment, 0)
@@ -33,45 +27,26 @@ func (o *Ollama) GenerateCommentsForPatch(diffs []*diff.FileDiff) ([]*models.Com
 			continue
 		}
 
-		log.Println("checking new diff", unidiff.NewName)
-
 		// now check all the hunks
-		for i, hunk := range unidiff.Hunks {
-			log.Println("checking hunk", i+1, "of", len(unidiff.Hunks))
+		for _, hunk := range unidiff.Hunks {
 			output, err := g.Chat(ctx, fmt.Sprintf(o.userPrompt, string(hunk.Body)))
 			if err != nil {
 				return nil, err
 			}
+
 			// convert to our struct
-			cmt := []*models.Comment{}
-			err = json.Unmarshal(removeJSONTag(output.Content), &cmt)
-			if err != nil {
-				log.Println(fmt.Errorf("error unmarshalling ollama response: %w", err))
-				pp.Println(removeJSONTag(output.Content))
-				err = os.WriteFile(filepath.Join(logFolder, fmt.Sprintf("F%vH%v", filepath.Base(unidiff.NewName), i)), []byte(output.Content), 0777)
-				if err != nil {
-					log.Println(err)
-				}
-				continue
+			cmt := &models.Comment{
+				FileName:  unidiff.NewName[2:],
+				StartLine: int(hunk.NewStartLine),
+				Comment:   output.Content,
 			}
 
-			// now fix all the lines etc
-			for _, c := range cmt {
-				affectedLineNumber := hunks.FindLineNumberInHunk(c.AffectedLine, string(hunk.Body))
-				if affectedLineNumber == -1 {
-					continue
-				}
-
-				c.FileName = unidiff.NewName[2:]
-				c.EndLine = int(hunk.NewStartLine) + affectedLineNumber
-			}
-
-			log.Println("adding", len(cmt), "new comments")
-			res = append(res, cmt...)
+			pp.Println(cmt)
+			res = append(res, cmt)
 		}
+
 	}
 
-	log.Println("found", len(res), "comments for this patch")
 	return res, nil
 }
 
@@ -95,8 +70,4 @@ func checkFilenameAllowed(filename string) bool {
 	}
 
 	return true
-}
-func removeJSONTag(input string) []byte {
-	j := strings.TrimPrefix(input, "```json")
-	return []byte(strings.TrimSuffix(j, "```"))
 }
